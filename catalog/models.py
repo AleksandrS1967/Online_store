6ea4fd07e32e2659efb4fb4aecb4071c773884e9
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-04-18 16:40:33 +0300

    Описал поля для моделей Product и Category. Связал продукт и категорию

--- catalog/models.py
+++ catalog/models.py
@@ -1,29 +1,32 @@
 from django.db import models
 
-
-# Create your models here.
-
-
-class Product(models.Model):
-    pass
-
-    # product_name = models.CharField(max_length=250, verbose_name='наименование')
-    # description = last_name = models.CharField(max_length=350, verbose_name='описание')
-    # images =
-
-    def __str__(self):
-        pass
-        # return f'{self.product_name} {self.description} {self.price}'
-
-    class Meta:
-        pass
+NULLABLE = {'blank': True, 'null': True}
 
 
 class Category(models.Model):
-    pass
+    product_name = models.CharField(max_length=250, verbose_name='наименование', help_text='Введите название товара')
+    description = models.TextField(verbose_name='описание')
 
     def __str__(self):
-        pass
+        return f'{self.product_name} {self.description}'
 
     class Meta:
-        pass
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=250, verbose_name='наименование', help_text='Введите название товара')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    images = models.ImageField(upload_to='catalog/images', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категозия')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_nou_add=True,  verbose_name='Дата создания(записи в БД',**NULLABLE)
+    updated_at = models.DateTimeField(auto_nou_add=True, verbose_name='Дата последнего изменения(записи в БД', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.product_name} {self.description} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
