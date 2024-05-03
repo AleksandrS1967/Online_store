6c7b140694d618c70da9e19af12af593cab0907e
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-05-04 00:26:51 +0300

    Перевёл имеющиеся контроллеры с FBV на CBV

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,9 @@ import json
 import os
 
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
@@ -10,17 +13,25 @@ from catalog.models import Product
 # Create your views here.
 
 
-def home(request):
-    products_list = Product.objects.all()
-    context = {
-        'product_list': products_list,
-        'title_name': 'Store',
-    }
-    return render(request, "catalog/home.html", context)
+class ProductListView(ListView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title_name'] = 'Online Store'
+        return context
 
 
-def contacts(request):
-    if request.method == "POST":
+class ContactsView(View):
+    @staticmethod
+    def get(request):
+        context = {
+            'title_name': 'Контакты',
+        }
+
+        return render(request, "catalog/contacts.html", context)
+
+    def post(self, request):
         email = request.POST.get("email")
         textarea = request.POST.get("textarea")
 
@@ -37,17 +48,36 @@ def contacts(request):
                     list_.append(data)
                 with open(json_f, "w") as f_1:
                     json.dump(list_, f_1)
-    context = {
-        'title_name': 'Контакты',
-    }
 
-    return render(request, "catalog/contacts.html", context)
+        return self.get(request)
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        'product': product,
-        'title_name': product.product_name,
-    }
-    return render(request, 'catalog/product_detail.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+    success_url = reverse_lazy('catalog:home')
+
+
+
+# def contacts(request):
+#     if request.method == "POST":
+#         email = request.POST.get("email")
+#         textarea = request.POST.get("textarea")
+#
+#         data = {"User": email, "Textarea": textarea}
+#
+#         json_f = "data.json"
+#
+#         with open(json_f, "a") as f:
+#             if os.stat(json_f).st_size == 0:
+#                 json.dump([data], f)
+#             else:
+#                 with open(json_f) as f_:
+#                     list_ = json.load(f_)
+#                     list_.append(data)
+#                 with open(json_f, "w") as f_1:
+#                     json.dump(list_, f_1)
+#     context = {
+#         'title_name': 'Контакты',
+#     }
+#
+#     return render(request, "catalog/contacts.html", context)
