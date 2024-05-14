2556498ba9a1ceb807ad27f1a90aed2643e79d80
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-05-14 22:34:17 +0300

    доработал модуль django.forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,9 +2,26 @@ from django import forms
 
 from catalog.models import Product
 
+list_valid = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
-        fields = ('product_name', 'category', 'price', 'images',)
+        fields = ('product_name', 'category', 'price', 'images', 'description')
+
+    def clean_product_name(self):
+        cleaned_data: str = self.cleaned_data.get('product_name')
+        for word in list_valid:
+            if word in cleaned_data.lower():
+                raise forms.ValidationError('название продукта не прошло валидацию')
+
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data: str = self.cleaned_data.get('description')
+        for word in list_valid:
+            if word in cleaned_data.lower():
+                raise forms.ValidationError('название продукта не прошло валидацию')
+
+        return cleaned_data
