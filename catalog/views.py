d84fd1e568ed9f1e771555d6b85690b8cd5cd8bc
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-05-14 21:16:11 +0300

    реализовал механизм CRUD, задействовав модуль django.forms

--- catalog/views.py
+++ catalog/views.py
@@ -5,8 +5,9 @@ import os
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 from catalog.utils.user_feedback import feedback
 
@@ -44,27 +45,19 @@ class ProductDetailView(DetailView):
     success_url = reverse_lazy('catalog:home')
 
 
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:home')
 
-# def contacts(request):
-#     if request.method == "POST":
-#         email = request.POST.get("email")
-#         textarea = request.POST.get("textarea")
-#
-#         data = {"User": email, "Textarea": textarea}
-#
-#         json_f = "data.json"
-#
-#         with open(json_f, "a") as f:
-#             if os.stat(json_f).st_size == 0:
-#                 json.dump([data], f)
-#             else:
-#                 with open(json_f) as f_:
-#                     list_ = json.load(f_)
-#                     list_.append(data)
-#                 with open(json_f, "w") as f_1:
-#                     json.dump(list_, f_1)
-#     context = {
-#         'title_name': 'Контакты',
-#     }
-#
-#     return render(request, "catalog/contacts.html", context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
+
